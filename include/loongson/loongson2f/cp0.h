eb520fd383b6ec9ccfedf50b894c28e9608ed4c2
xiangy <xiangy@lemote.com>
2009-08-17 09:56:11 +0800

    1.uart is work now

--- include/loongson/loongson2f/cp0.h
+++ include/loongson/loongson2f/cp0.h
@@ -2,33 +2,38 @@
  * according to Loongson2F user manual
  *
  */
-#define CP0_TLB_INDEX		$0
-#define CP0_TLB_RANDOM		$1
-#define CP0_TLB_LO0			$2
-#define CP0_TLB_LO1			$3
-#define CP0_TLB_CONTEXT		$4
-#define CP0_TLB_PG_MASK		$5
-#define CP0_TLB_WIRED		$6
+#define CP0_INDEX		$0
+#define CP0_RANDOM		$1
+#define CP0_ENTRYLO0	$2
+#define CP0_ENTRYLO1	$3
+#define CP0_CONTEXT		$4
+#define CP0_PAGEMASK	$5
+#define CP0_WIRED		$6
 /*reg 7 reserved*/
-#define CP0_BAD_VADDR		$8
-#define CP0_COUNT			$9
-#define CP0_TLB_ENTRY_HI	$10
-#define CP0_COMPARE			$11
-#define CP0_STATUS			$12
-#define CP0_CAUSE			$13
-#define CP0_EPC				$14
-#define CP0_PRID			$15
-#define CP0_CONFIG			$16
-#define CP0_LLADDR			$17
-#define CP0_WATCH			$18
+#define CP0_BADVADDR	$8
+#define CP0_COUNT		$9
+#define CP0_ENTRYHI		$10
+#define CP0_COMPARE		$11
+#define CP0_STATUS		$12
+#define CP0_CAUSE		$13
+#define CP0_EPC			$14
+#define CP0_PRID		$15
+#define CP0_CONFIG		$16
+#define CP0_LLADDR		$17
+#define CP0_WATCH		$18
 /*reg 19 reserved*/
-#define CP0_TLB_XCONTEXT	$20
+#define CP0_XCONTEXT	$20
 /*reg 21 reserved*/
-#define CP0_DIAGNOSE		$22
+#define CP0_DIAGNOSE	$22
 /*reg 23 reserved*/
-#define CP0_PCLO			$24
-#define CP0_PCHI			$25
+#define CP0_PCLO		$24
+#define CP0_PCHI		$25
 /*reg 26,27 reserved*/
-#define CP0_TAG_LO			$28
-#define CP0_TAG_HI			$29
-#define CP0_ERROREPC		$30
+#define CP0_TAGLO		$28
+#define CP0_TAGHI		$29
+#define CP0_ERROREPC	$30
+
+/**** status register bit define *****/
+#define STATUS_IE		1<<0
+#define STATUS_KSU		3<<3
+#define STATUS_BEV		1<<22
--- include/loongson/loongson2f/uart.h
+++ include/loongson/loongson2f/uart.h
@@ -1,1 +1,2 @@
-#define LOONSON2F_UART_ADDR 0xffffffff1ff003f8
+#define UART_ADDR 0xffffffff1ff003f8
+#define UART_BAUD 3686400
