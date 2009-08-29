eea930219cd3110c7ca26f8fad307dc95fbddc8d
xiangy <xiangy@lemote.com>
2009-08-29 18:29:36 +0800

    add rom speed

--- include/loongson/loongson2f/nb_reg.h
+++ include/loongson/loongson2f/nb_reg.h
@@ -2,6 +2,7 @@
 #define	NBIO_CTRL_REG		0x1fe00100 /*NBridge register,except pci config header*/
 #define PCICFG_SPACE		0x1fe80000/* all pci config space access by rd/wr this space*/
 
+#define LIO_CFG		(NBIO_CTRL_REG + 0x08)
 #define PCIMAP_CFG	(NBIO_CTRL_REG + 0x18)		
 #define CHIP_CFG	(NBIO_CTRL_REG + 0X80)
 
