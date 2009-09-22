b2bef5e5880f07dcffa970614c5f113294890beb
xiangy <xiangy@lemote.com>
2009-09-22 10:28:11 +0800

    early_printf support %d,%x,%i now, but the code is not extensible

--- init/main.c
+++ init/main.c
@@ -11,12 +11,10 @@ void main(void)
 	test[3] = '\n';
 	test[4] = '\0';
 	early_printf("are you right? %% %s xixi\n","ni hao ma");
-	early_printf("are you right? \n");
-	early_printf("are you right? \n");
-	early_printf("are you right? \n");
-	early_printf("are you right? \n");
-	early_printf("are you right? \n");
-	early_printf("are you right? \n");
+	early_printf("are %x you %c righ%c t? \n", 1024, 'A', 'B');
+	early_printf("are you %s right? \n","shi bu shi\n");
+	early_printf("are%s you %d rig%i ht? \n", "no no ", 123, -890);
+	early_printf("are you %x right? \n",0x1234dfe);
 	while(1);
 	asm (".set mips3\n dli $2, 0xffffffffbfc00000;jalr $2":::"$2");
 }
--- lib/printf.c
+++ lib/printf.c
@@ -1,19 +1,54 @@
 #include <stdarg.h>
 #include <string.h>
 
-#define SINT	(1 << 0) 		
-#define USINT	(1 << 1)
-#define SLONG	(1 << 2)
-#define USLONG	(1 << 3)
+#define USCHAR	(1 << 0)
+#define SCHAR	(1 << 1)
+#define USSHORT	(1 << 2)
+#define SSHORT	(1 << 3)
+#define SINT	(1 << 4) 		
+#define USINT	(1 << 5)
+#define SLONG	(1 << 6)
+#define USLONG	(1 << 7)
 
-static char *number(char *buf, char *end, unsigned long long num, int base , u32 flags) 
+#define BASE2	(1 << 16)
+#define BASE8	(1 << 17)
+#define BASE10  (1 << 18)
+#define BASE16  (1 << 19)
+
+static char *number(char *buf, char *end, unsigned long long num, u32 flags) 
 {
 	static const char digits[16] = "0123456789ABCDEF";
+	char tmp[66]; // 1(signed) + 64(digits) + 1 
+	int sign = 0, i;
 
-	if(flags & SINT) {
-		if(1);	
+	if(flags & (SINT | SLONG)) {
+		sign = 1;
+	} 
+
+	i = 0;
+	if(flags & BASE10) {
+		if(sign && ((signed long)num < 0)) {
+			if((buf + 1) < end)
+				*buf++ = '-'; 
+			num = 0 - num;
+		}
+
+		/* NOTICE: if remove u32 in following 3 lines,compiler error*/
+		/* this will have problem when long is 64 bits */
+		do {
+			tmp[i++] = digits[(u32)num % 10];
+		} while(num = ((u32)num / 10));	
+	} else if(flags & BASE16) {
+		do {
+			tmp[i++] = digits[num & 0xf];
+		} while(num = (num >> 4));		
 	}
-
+	
+	if (buf + i < end) {
+		while(i) {
+			*buf++ = tmp[--i];
+		}
+	}
 	return buf;
 }
 /*
@@ -24,8 +59,7 @@ int parse_string(char buf[], int size, const char * fmt, va_list args)
 {
 	char c;
 	char *str, *end ,*output;
-	int base = 10;		// set default base to 10 
-	u32 flags = USINT;  // set default flag to unsigned long
+	u32 flags ;
 	unsigned long long num;
 	size_t len;
 
@@ -63,15 +97,16 @@ int parse_string(char buf[], int size, const char * fmt, va_list args)
 			continue;
 		case 'd':
 		case 'i':
-			flags |= SINT;
+			flags = BASE10 | SINT;
 			break;
 		case 'u':
-			flags |= USINT;	
+			flags = BASE10 | USINT;	
 			break;
 		case 'x':
-			base = 16;
-		default:
+			flags = BASE16 | USINT;	
 			break;
+		default:
+			flags = (USINT | BASE10);
 		}
 
 		/* only to deal with int/unsigned int now */
@@ -82,7 +117,7 @@ int parse_string(char buf[], int size, const char * fmt, va_list args)
 			num = (signed int)va_arg(args, int);
 		}
 		
-		str = number(str, end, num, base, flags);
+		str = number(str, end, num, flags);
 	}	
 
 	return str - buf;
