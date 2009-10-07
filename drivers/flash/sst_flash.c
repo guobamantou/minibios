17301557091e86ea1f82a048f923ba505214e3e7
xiangy <xiangy@lemote.com>
2009-10-07 10:00:33 +0800

    add flash erase and program state probe, not test yet

--- drivers/flash/sst_flash.c
+++ drivers/flash/sst_flash.c
@@ -1,6 +1,29 @@
 #include <io.h>
 #include <types.h>
 
+int sst_erase_busy(u32 offset)
+{
+	u8 poll;
+	u32 flash_addr = 0xbfc00000;
+
+	poll = inb(flash_addr + offset);	
+	if(poll & 0x80) // see sst flash datasheet Data# Polling (DQ7) 
+		return 1;
+	return 0;	
+}
+
+int sst_program_busy()
+{
+	u8 poll1, poll2;
+	u32 flash_addr = 0xbfc00000;
+
+	poll1 = inb(flash_addr + offset);	
+	poll2 = inb(flash_addr + offset);	
+	if((poll1 ^ poll2) & 0x40) // set sst flash datasheet Toggle Bit(DQ6)
+		return 1;
+	return 0;
+}
+
 void sst_erase_sector(u32 offset)
 {
 	u32 flash_addr = 0xbfc00000;
@@ -14,6 +37,7 @@ void sst_erase_sector(u32 offset)
 	
 	return ;
 }
+
 void sst_erase_chip()
 {
 	u32 flash_addr = 0xbfc00000;
