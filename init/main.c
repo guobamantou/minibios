8510fc03cdd23ec8a9cc5158126a88d8b2341227
xiangy <xiangy@lemote.com>
2009-10-27 11:10:24 +0800

    malloc is ok now

--- init/main.c
+++ init/main.c
@@ -6,11 +6,13 @@
 extern int boot_flash_init(struct flash_device *);
 extern void init_cpu_freq(void);
 extern u32 cpu_freq;
+extern void *malloc(size_t);
 
 void main(void)
 {
 	int i;
 	struct flash_device flash_dev;
+	char *p;
 
 	i = boot_flash_init(&flash_dev);
 	if(i == 0)
@@ -19,7 +21,12 @@ void main(void)
 	init_cpu_freq();
 
 	printf("cpu freq is %d\n", cpu_freq);
-
+	p = malloc(100);
+	printf("p is %x\n", p);
+	p = malloc(0x10000);
+	printf("p is %x\n", p);
+	p = malloc(12);
+	printf("p is %x\n", p);
 	while(1);
 	asm (".set mips3\n dli $2, 0xffffffffbfc00000;jalr $2":::"$2");
 }
--- mm/malloc.c
+++ mm/malloc.c
@@ -10,43 +10,142 @@
 #define MALLOC_END_ADDR		(PHY_TO_CACHED(224*1024*1024))	
 
 struct mem_list {
-	ulong next;
-	ulong status;
-};
+	char status;
+	size_t size; //unit: Byte
+	struct mem_list *next;
+} __attribute__((aligned(16)));
+
 #define MEM_USED 1
 #define MEM_FREE 0
+
+#define SIZEOF_MEMLIST 16		// struct mem_list is 16 Bytes
+struct mem_list mem_free_list;	// mem_free_list and mem_used_list are all list header
+struct mem_list mem_used_list;
 
 int malloc_inited = 0;
 #define MALLOC_INITED 1
 
-ulong last_addr;    
 ulong cur_addr;		// current addr avilable for allocate 
+
+static void list_add(struct mem_list *, struct mem_list *);
+static void list_del(struct mem_list *, struct mem_list *);
+static void print_list(struct mem_list *head);
 
 static void malloc_init(void)
 {
 	struct mem_list *temp;
-
 	if(malloc_inited == MALLOC_INITED)
 		return ;	
+	
+	temp = (struct mem_list *)MALLOC_START_ADDR;
+	temp->next = NULL;
+	temp->size = MALLOC_END_ADDR - MALLOC_START_ADDR - SIZEOF_MEMLIST; 
 
-	temp = (struct mem_list *)(MALLOC_START_ADDR);
-	if((ulong)temp & 0Xf) {	// mem_list structure should be 16 byte aligned 
-		temp->next = MALLOC_END_ADDR;			
-		temp->status = MEM_FREE;			
-	} else {
-		panic("malloc start addr not aligned!");
-	}
-	last_addr = 0x0; // invalid addr
-	cur_addr  = MALLOC_START_ADDR;
+	mem_used_list.next = NULL;
+	mem_used_list.size = 0;
+	mem_free_list.next = temp;
+	mem_free_list.size = 0; 
 	malloc_inited = MALLOC_INITED;
 }
 
 void * malloc(size_t size)
 {
-	return (void *)NULL;
+	struct mem_list *p, *q;
+
+	if(malloc_inited != MALLOC_INITED)
+		malloc_init();	
+
+	if(size < 16)
+		size = 16;
+	size = (size + 15) & ~(0xf);
+
+	p = mem_free_list.next;
+	while(p){
+		if(p->size >= size) {
+			break;
+		} else {
+			p = p->next;
+		}
+	}
+
+	if(p) {
+		list_del(&mem_free_list, p);
+
+		if(p->size - size > 128) { // we don't like fragment	
+			q = (struct mem_list *)((ulong)p + SIZEOF_MEMLIST + size); 	
+			q->size = p->size - 2 * SIZEOF_MEMLIST - size;
+			p->size = size;
+			list_add(&mem_free_list, q);
+		}
+		list_add(&mem_used_list, p);
+	} else {
+	//	reclaim_mem();
+		printf("out of memory!");
+		return (void *)NULL;
+	}
+#if 1
+	printf("free\n");
+	print_list(&mem_free_list);
+	printf("used\n");
+	print_list(&mem_used_list);
+#endif
+	return (void *)((ulong)p + SIZEOF_MEMLIST);
 }
 
 void free(void * ptr)
 {
 
+}
+
+static void list_del(struct mem_list *head, struct mem_list *del)
+{
+	struct mem_list *p;
+
+	if((del == NULL) || (head == NULL) || (head->next == NULL))
+		return;	
+	
+	for(p = head; p ; p = p->next){
+		if(p->next == del){
+			p->next = del->next; // not free anything here
+			return ;
+		}	
+	}	
+}
+
+static void list_add(struct mem_list *head, struct mem_list *new)
+{
+	struct mem_list *cur, *pre;
+
+	if((new == NULL) || (head == NULL))
+		return;	
+	
+	pre = head;
+	cur = head->next;
+
+	while(1){
+		if((cur == NULL) || (cur > new)){
+			new->next = pre->next;
+			pre->next = new; 
+			return ;
+		}	
+		pre = pre->next;
+		cur = cur->next;
+	}	
+}
+
+static void print_list(struct mem_list *head)
+{
+	struct mem_list *p;
+
+	if(head && head->next)
+		p = head->next;
+	else 	
+		return ;
+
+	while(p){
+		printf("addr is %x\n", p);
+		printf("next is %x\n", p->next);
+		printf("size is %x\n", p->size);
+		p = p->next;
+	}
 }
